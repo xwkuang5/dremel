102b5e3fa64a608e3d0217de3ee007ddbc6f379d
xwkuang5 <louisconfussing@gmail.com>
2025-11-29 12:50:40 -0500

    Autopep8, requirements.txt, pre-commit hooks etc.

--- dremel_shred.py
+++ dremel_shred.py
@@ -1,5 +1,6 @@
 import collections
 
+
 class ColumnDescriptor:
     def __init__(self, path, parent=None, max_repetition_level=0, max_definition_level=0):
         self.path = path
@@ -17,14 +18,15 @@ class ColumnDescriptor:
         return self.children[path]
 
     def compute_levels(self, current_rep_level=0, current_def_level=0):
-        new_def_level = current_def_level + (1 if self.parent is not None else 0)
+        new_def_level = current_def_level + \
+            (1 if self.parent is not None else 0)
         new_rep_level = current_rep_level
         if self.is_repeated:
             new_rep_level += 1
-            
+
         self.max_repetition_level = new_rep_level
         self.max_definition_level = new_def_level
-        
+
         for child in self.children.values():
             child.compute_levels(new_rep_level, new_def_level)
 
@@ -44,39 +46,41 @@ class ColumnDescriptor:
                 f"is_repeated={self.is_repeated}, "
                 f"children={list(self.children.values())})")
 
+
 class FieldWriter:
     def __init__(self, descriptor):
         self.descriptor = descriptor
         self.children = collections.OrderedDict()
-        self.data = [] # List of (value, r, d)
-        
+        self.data = []  # List of (value, r, d)
+
         for name, child_desc in descriptor.children.items():
             self.children[name] = FieldWriter(child_desc)
-            
+
     @property
     def name(self):
         return self.descriptor.path
-        
+
     @property
     def is_repeated(self):
         return self.descriptor.is_repeated
-        
+
     @property
     def max_repetition_level(self):
         return self.descriptor.max_repetition_level
-        
+
     @property
     def max_definition_level(self):
         return self.descriptor.max_definition_level
-        
+
     def get_child(self, name):
         return self.children.get(name)
-        
+
     def is_leaf(self):
         return len(self.children) == 0
-        
+
     def write(self, value, r, d):
         self.data.append((value, r, d))
+
 
 class RecordDecoder:
     def __init__(self, record, definition_level):
@@ -84,12 +88,12 @@ class RecordDecoder:
         self.definition_level = definition_level
         self.iterator = None
         self._prepare_iterator()
-        
+
     def _prepare_iterator(self):
         if self.record is None:
             self.iterator = iter([])
             return
-            
+
         if isinstance(self.record, dict):
             # Generator to yield (key, value)
             # If value is list, yield (key, item) for each item
@@ -120,6 +124,7 @@ class RecordDecoder:
             return item
         return next(self.iterator)
 
+
 def dissect_record(decoder, writer, repetition_level):
     """
     Dissects a record into a set of columns.
@@ -129,10 +134,10 @@ def dissect_record(decoder, writer, repetition_level):
     - Eagerly writes all leaves (i.e., not sparse)
     """
     seen_fields = set()
-    
+
     while decoder.has_next():
         field, value = decoder.next()
-        
+
         child_writer = writer.get_child(field)
         if not child_writer:
             continue
@@ -142,9 +147,10 @@ def dissect_record(decoder, writer, repetition_level):
             child_repetition_level = child_writer.max_repetition_level
         else:
             seen_fields.add(field)
-            
+
         if child_writer.is_leaf():
-            child_writer.write(value, child_repetition_level, decoder.definition_level + 1)
+            child_writer.write(value, child_repetition_level,
+                               decoder.definition_level + 1)
         else:
             new_dec = RecordDecoder(value, decoder.definition_level + 1)
             dissect_record(new_dec, child_writer, child_repetition_level)
@@ -152,10 +158,12 @@ def dissect_record(decoder, writer, repetition_level):
     for field, child_writer in writer.children.items():
         if field not in seen_fields:
             if child_writer.is_leaf():
-                child_writer.write(None, repetition_level, decoder.definition_level)
+                child_writer.write(None, repetition_level,
+                                   decoder.definition_level)
             else:
                 new_dec = RecordDecoder(None, decoder.definition_level)
                 dissect_record(new_dec, child_writer, repetition_level)
+
 
 def parse_schema(schema_paths):
     root = ColumnDescriptor("$")
@@ -172,28 +180,30 @@ def parse_schema(schema_paths):
     root.compute_levels()
     return root
 
+
 def shred_records(schema_paths, records):
     root_descriptor = parse_schema(schema_paths)
     root = FieldWriter(root_descriptor)
     for record in records:
         decoder = RecordDecoder(record, definition_level=0)
         dissect_record(decoder, root, repetition_level=0)
-        
+
     output = {}
+
     def collect(node, path_prefix):
         path = node.name
         if node.is_repeated:
             path += "[*]"
         full_path = f"{path_prefix}.{path}" if path_prefix else path
-        
+
         if node.is_leaf():
             clean_path = full_path.replace("$.", "")
             output[clean_path] = node.data
-        
+
         for child in node.children.values():
             collect(child, full_path)
-            
+
     for child in root.children.values():
         collect(child, "")
-        
+
     return output
--- dremel_shred_test.py
+++ dremel_shred_test.py
@@ -1,14 +1,17 @@
 import unittest
 from dremel_shred import shred_records, parse_schema, ColumnDescriptor
 
+
 def mk_desc(path, r, d, is_repeated=False, children=None):
-    desc = ColumnDescriptor(path, parent=None, max_repetition_level=r, max_definition_level=d)
+    desc = ColumnDescriptor(
+        path, parent=None, max_repetition_level=r, max_definition_level=d)
     desc.is_repeated = is_repeated
     if children:
         for child in children:
             desc.children[child.path] = child
             child.parent = desc
     return desc
+
 
 class TestDremelShred(unittest.TestCase):
     def test_basic_example(self):
@@ -18,17 +21,19 @@ class TestDremelShred(unittest.TestCase):
             {"a": {"d": 2}}
         ]
         result = shred_records(schema, records)
-        self.assertEqual(result["a.b[*].c"], [(1, 0, 3), (2, 1, 3), (None, 0, 1)])
+        self.assertEqual(result["a.b[*].c"],
+                         [(1, 0, 3), (2, 1, 3), (None, 0, 1)])
         self.assertEqual(result["a.d"], [(1, 0, 2), (2, 0, 2)])
 
     def test_paper_example(self):
-        schema = ["DocId", "Links.Backward[*]", "Links.Forward[*]", "Name[*].Language[*].Code", "Name[*].Language[*].Country", "Name[*].Url"]
+        schema = ["DocId", "Links.Backward[*]", "Links.Forward[*]",
+                  "Name[*].Language[*].Code", "Name[*].Language[*].Country", "Name[*].Url"]
         records = [
             {
-                "DocId": 10, 
+                "DocId": 10,
                 "Links": {
                     "Forward": [20, 40, 60]
-                }, 
+                },
                 "Name": [
                     {
                         "Language": [
@@ -37,20 +42,20 @@ class TestDremelShred(unittest.TestCase):
                         ],
                         "Url": "http://A"
                     },
-                    { 
-                        "Url": "http://B" 
+                    {
+                        "Url": "http://B"
                     },
-                    { 
-                        "Language": [{"Code": "en-gb", "Country": "gb"}] 
+                    {
+                        "Language": [{"Code": "en-gb", "Country": "gb"}]
                     }
                 ]
             },
             {
-                "DocId": 20, 
+                "DocId": 20,
                 "Links": {
                     "Backward": [10, 30],
                     "Forward": [80]
-                }, 
+                },
                 "Name": [
                     {"Url": "http://C"}
                 ]
@@ -58,8 +63,10 @@ class TestDremelShred(unittest.TestCase):
         ]
         result = shred_records(schema, records)
         self.assertEqual(result["DocId"], [(10, 0, 1), (20, 0, 1)])
-        self.assertEqual(result["Name[*].Url"], [("http://A", 0, 2), ("http://B", 1, 2), (None, 1, 1), ("http://C", 0, 2)])
-        self.assertEqual(result["Name[*].Language[*].Code"], [("en-us", 0, 3), ("en", 2, 3), (None, 1, 1), ("en-gb", 1, 3), (None, 0, 1)])
+        self.assertEqual(result["Name[*].Url"], [("http://A", 0, 2),
+                         ("http://B", 1, 2), (None, 1, 1), ("http://C", 0, 2)])
+        self.assertEqual(result["Name[*].Language[*].Code"], [("en-us", 0, 3),
+                         ("en", 2, 3), (None, 1, 1), ("en-gb", 1, 3), (None, 0, 1)])
 
     def test_missing_root(self):
         schema = ["a.b"]
@@ -93,21 +100,24 @@ class TestDremelShred(unittest.TestCase):
     def test_multiple_records_mixed(self):
         schema = ["doc.links[*].forward", "doc.links[*].backward"]
         records = [
-            {"doc": {"links": [{"forward": 20, "backward": 10}, {"forward": 40}]}},
+            {"doc": {
+                "links": [{"forward": 20, "backward": 10}, {"forward": 40}]}},
             {"doc": {"links": [{"backward": 30}]}}
         ]
         # doc(1).links(2).forward(3)/backward(3)
-        
+
         # Record 1:
         # Link 1: fwd=20 (d=3, r=0), back=10 (d=3, r=0)
         # Link 2: fwd=40 (d=3, r=1), back=missing (d=2, r=1)
-        
+
         # Record 2:
         # Link 1: fwd=missing (d=2, r=0), back=30 (d=3, r=0)
-        
+
         result = shred_records(schema, records)
-        self.assertEqual(result["doc.links[*].forward"], [(20, 0, 3), (40, 1, 3), (None, 0, 2)])
-        self.assertEqual(result["doc.links[*].backward"], [(10, 0, 3), (None, 1, 2), (30, 0, 3)])
+        self.assertEqual(result["doc.links[*].forward"],
+                         [(20, 0, 3), (40, 1, 3), (None, 0, 2)])
+        self.assertEqual(result["doc.links[*].backward"],
+                         [(10, 0, 3), (None, 1, 2), (30, 0, 3)])
 
     def test_shred_repeated_leaf_mixed(self):
         schema = ["data.values[*]", "data.meta"]
@@ -119,42 +129,43 @@ class TestDremelShred(unittest.TestCase):
         ]
         # data.values[*]: r=1, d=2
         # data.meta: r=0, d=2
-        
+
         # Record 1: values=[1, 2], meta="m1"
         # values: (1, 0, 2), (2, 1, 2)
         # meta: ("m1", 0, 2)
-        
+
         # Record 2: values=[], meta="m2"
         # values: (None, 0, 1)  <- data present(1), values missing(1)
         # meta: ("m2", 0, 2)
-        
+
         # Record 3: data present, empty
         # values: (None, 0, 1)
         # meta: (None, 0, 1)
-        
+
         # Record 4: missing root
         # values: (None, 0, 0)
         # meta: (None, 0, 0)
-        
+
         result = shred_records(schema, records)
         self.assertEqual(result["data.values[*]"], [
-            (1, 0, 2), (2, 1, 2), 
-            (None, 0, 1), 
-            (None, 0, 1), 
+            (1, 0, 2), (2, 1, 2),
+            (None, 0, 1),
+            (None, 0, 1),
             (None, 0, 0)
         ])
         self.assertEqual(result["data.meta"], [
-            ("m1", 0, 2), 
-            ("m2", 0, 2), 
-            (None, 0, 1), 
+            ("m1", 0, 2),
+            ("m2", 0, 2),
+            (None, 0, 1),
             (None, 0, 0)
         ])
+
 
 class TestParseSchema(unittest.TestCase):
     def test_simple_schema(self):
         schema = ["a", "b"]
         root = parse_schema(schema)
-        
+
         expected = mk_desc("$", 0, 0, children=[
             mk_desc("a", 0, 1),
             mk_desc("b", 0, 1)
@@ -164,7 +175,7 @@ class TestParseSchema(unittest.TestCase):
     def test_nested_schema(self):
         schema = ["a.b"]
         root = parse_schema(schema)
-        
+
         expected = mk_desc("$", 0, 0, children=[
             mk_desc("a", 0, 1, children=[
                 mk_desc("b", 0, 2)
@@ -175,7 +186,7 @@ class TestParseSchema(unittest.TestCase):
     def test_repeated_schema(self):
         schema = ["a[*].b"]
         root = parse_schema(schema)
-        
+
         expected = mk_desc("$", 0, 0, children=[
             mk_desc("a", 1, 1, is_repeated=True, children=[
                 mk_desc("b", 1, 2)
@@ -186,7 +197,7 @@ class TestParseSchema(unittest.TestCase):
     def test_repeated_leaf_schema(self):
         schema = ["a.b[*]"]
         root = parse_schema(schema)
-        
+
         expected = mk_desc("$", 0, 0, children=[
             mk_desc("a", 0, 1, children=[
                 mk_desc("b", 1, 2, is_repeated=True)
@@ -195,9 +206,10 @@ class TestParseSchema(unittest.TestCase):
         self.assertEqual(root, expected)
 
     def test_paper_schema(self):
-        schema = ["DocId", "Links.Backward[*]", "Links.Forward[*]", "Name[*].Language[*].Code", "Name[*].Language[*].Country", "Name[*].Url"]
+        schema = ["DocId", "Links.Backward[*]", "Links.Forward[*]",
+                  "Name[*].Language[*].Code", "Name[*].Language[*].Country", "Name[*].Url"]
         root = parse_schema(schema)
-        
+
         expected = mk_desc("$", 0, 0, children=[
             mk_desc("DocId", 0, 1),
             mk_desc("Links", 0, 1, children=[
@@ -213,6 +225,7 @@ class TestParseSchema(unittest.TestCase):
             ])
         ])
         self.assertEqual(root, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
